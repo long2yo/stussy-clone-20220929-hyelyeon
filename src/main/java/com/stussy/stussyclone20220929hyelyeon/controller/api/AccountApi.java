656afbd7a9e1c853602165ec68a5929c2ee67f9f
long2yo <long2yo@naver.com>
2022-10-05 22:22:24 +0900

    Validation

--- src/main/java/com/stussy/stussyclone20220929hyelyeon/controller/api/AccountApi.java
+++ src/main/java/com/stussy/stussyclone20220929hyelyeon/controller/api/AccountApi.java
@@ -1,9 +1,18 @@
 package com.stussy.stussyclone20220929hyelyeon.controller.api;
 
+import com.stussy.stussyclone20220929hyelyeon.dto.CMRespDto;
 import com.stussy.stussyclone20220929hyelyeon.dto.account.RegisterReqDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RequestMapping("/api/account")
@@ -11,10 +20,55 @@ import org.springframework.web.bind.annotation.*;
 public class AccountApi {
 
     @PostMapping("/register")//RegisterReqDto - date를 받음
-    public ResponseEntity<?> register(@RequestBody RegisterReqDto registerReqDto) {
+    public ResponseEntity<?> register(@Valid @RequestBody RegisterReqDto registerReqDto, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()) {
+            log.error("유효성 검사 오류가 발생");
+            Map<String, String> errorMap = new HashMap<String, String>();
+
+            List<List<FieldError>> codeList = new ArrayList<List<FieldError>>();
+            codeList.add(new ArrayList<FieldError>());  //Pattern
+            codeList.add(new ArrayList<FieldError>());  //NotBlank
+
+
+            bindingResult.getFieldErrors().forEach(error -> {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+
+               if(error.getCode().equals("Pattern")) {
+                   codeList.get(0).add(error);
+               }else if(error.getCode().equals("NotBlank")) {
+                   codeList.get(1).add(error);
+               }
+            });
+
+            log.info("codeList: {}", codeList);
+
+            codeList.forEach(errorMapList -> {
+                errorMapList.forEach(error -> {
+                    errorMap.put(error.getField(), error.getDefaultMessage());
+                });
+                log.info("error: {}", errorMap);
+            });
+
+
+//            bindingResult.getFieldErrors().forEach(error -> {
+//                log.info("Error: 코드({}), 필드명({}), 메세지({})", error.getCode(), error.getField(), error.getDefaultMessage());
+//                if(!error.getCode().equals("NotBlank")) {
+//                    errorMap.put(error.getField(), error.getDefaultMessage());
+//                }
+//            });
+//            bindingResult.getFieldErrors().forEach(error -> {
+//                log.info("Error: 코드({}), 필드명({}), 메세지({})", error.getCode(), error.getField(), error.getDefaultMessage());
+//                if(error.getCode().equals("NotBlank")) {
+//                    errorMap.put(error.getField(), error.getDefaultMessage());
+//                }
+//            });
+            return ResponseEntity.badRequest().body(new CMRespDto<>(-1, "유효성 검사 실패", errorMap));
+        }
 
         log.info("{}", registerReqDto);
 
         return  ResponseEntity.ok(null);
     }
+
 }
--- src/main/java/com/stussy/stussyclone20220929hyelyeon/dto/account/RegisterReqDto.java
+++ src/main/java/com/stussy/stussyclone20220929hyelyeon/dto/account/RegisterReqDto.java
@@ -3,6 +3,7 @@ package com.stussy.stussyclone20220929hyelyeon.dto.account;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 @Data
@@ -10,11 +11,16 @@ public class RegisterReqDto {
 
     @Pattern(regexp = "^[가-힇]{1,3}$", message = "이름은 한글만 입력가능하며 한글자 이상 세글자 이하로 작성하세요")
     private String lastName;
+
     @Pattern(regexp = "^[가-힇]{1,2}$", message = "성은 한글자만 입력가능하며 한글자 이상 두글자 이하로 작성하세요")
     private String firstName;
-    @Email(message = "이메일은 비워둘수 없습니다.")
+
+    @Email(message = "이메일은 비워둘수 없습니다.")//(message = "이메일은 비워둘수 없습니다.")생략가능 - @NotBlank
+    @NotBlank(message = "이메일은 비워 둘 수 없습니다.")//비워둘수 없다
     private String email;
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[~!@#$%^&*_])[a-zA-Z\\d-~!@#$%^&*_]{8,16}$")
+
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[~!@#$%^&*_])[a-zA-Z\\d-~!@#$%^&*_]{8,16}$", message = "비밀번호는 숫자, 영문(대소문자), 특수기호를 하나 이상 포함하여야하며 8자 이상 16자 이하로 작성해야 합니다")
+    @NotBlank(message = "비밀번호는 비워 둘 수 없습니다.")
     private String password;
 
 
